c28aa255e5faa3e81f642879a4fa809eca388edf
David Ruiz Urraca <davidru85@gmail.com>
2022-05-31 11:54:30 +0200

    Moves mainactivity to refactored layer package

--- app/src/main/java/com/ruizurraca/reservationappdemo/presentation/MainActivity.kt
+++ app/src/main/java/com/ruizurraca/reservationappdemo/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.ruizurraca.reservationappdemo
+package com.ruizurraca.reservationappdemo.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ruizurraca.reservationappdemo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
